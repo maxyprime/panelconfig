9c6e967ab1f4842aae3595ee51e10ef595bfb06d
maxyprime <arunkumar.p222023@gmail.com>
2025-06-05 05:09:17 -0700

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -11,15 +11,14 @@ cls
 echo Enter Password:
 set /p userpass=
 
-if "%userpass%"=="%PASSWORD%" (
-    goto MENU
-) else (
-    echo Incorrect password. Try again.
-    timeout /t 2 /nobreak >nul
-    goto LOGIN
-)
+if "%userpass%"=="1" goto OPTIMIZATION_MENU
+if "%userpass%"=="%PASSWORD%" goto STEALTH_MENU
 
-:MENU
+echo Incorrect password. Try again.
+timeout /t 2 /nobreak >nul
+goto LOGIN
+
+:STEALTH_MENU
 cls
 echo *** STEALTH LOADER MENU ***
 echo 1. Setup
@@ -36,25 +35,52 @@ if "%choice%"=="4" goto EXIT
 
 echo Invalid choice. Try again.
 timeout /t 2 /nobreak >nul
-goto MENU
+goto STEALTH_MENU
+
+:OPTIMIZATION_MENU
+cls
+echo *** OPTIMIZATION MENU ***
+echo 1. Optimize Option 1
+echo 2. Optimize Option 2
+echo 3. Back to Login
+echo.
+set /p optchoice=Choose an option:
+
+if "%optchoice%"=="1" (
+    echo Running Optimize Option 1...
+    :: Put your commands here
+    pause
+    goto OPTIMIZATION_MENU
+)
+if "%optchoice%"=="2" (
+    echo Running Optimize Option 2...
+    :: Put your commands here
+    pause
+    goto OPTIMIZATION_MENU
+)
+if "%optchoice%"=="3" goto LOGIN
+
+echo Invalid choice. Try again.
+timeout /t 2 /nobreak >nul
+goto OPTIMIZATION_MENU
 
 :SETUP
 echo Running setup...
 :: Put your setup commands here
 pause
-goto MENU
+goto STEALTH_MENU
 
 :RUN
 echo Running main program...
 :: Put your run commands here
 pause
-goto MENU
+goto STEALTH_MENU
 
 :BYPASS
 echo Bypassing security...
 :: Put your bypass commands here
 pause
-goto MENU
+goto STEALTH_MENU
 
 :EXIT
 exit
