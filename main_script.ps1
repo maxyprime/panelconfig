92750b41246c2dc708c5e85370dec2b435376895
maxyprime <arunkumar.p222023@gmail.com>
2025-06-06 10:38:33 -0700

    Add files via upload

---
+++ main_script.ps1
@@ -0,0 +1,7 @@
+# main_script.ps1
+Write-Host ""
+Write-Host "======================================" -ForegroundColor Cyan
+Write-Host "       STEALTH LOADER ACTIVATED       " -ForegroundColor Green
+Write-Host "   PowerShell logic is running...     " -ForegroundColor Yellow
+Write-Host "======================================" -ForegroundColor Cyan
+Start-Sleep -Seconds 5
